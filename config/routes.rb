c8ac1437c94771015fef44729f32e4ece974d4e3
tanahara <s10.vermouth.4869@gmail.com>
2020-08-04 19:41:30 +0900

    topコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: "top#index"
   devise_for :users, controllers: {
-            omniauth_callbacks: "omniauth_callbacks",
-          }
+                       omniauth_callbacks: "omniauth_callbacks",
+                     }
 end
